cdae0dec6f67d42e64511db8e52fc30573cb489f
br1xn <brianbiju@gmail.com>
2024-07-02 12:00:17 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,28 @@
+# Getting the sum of variable of linear Equation
 from nada_dsl import *
-
 
 def nada_main():
     party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    a = SecretInteger(Input(name="my_int1", party=party1))
+    b = SecretInteger(Input(name="my_int2", party=party1))
+    c = SecretInteger(Input(name="my_int3", party=party1))
+    d = SecretInteger(Input(name="my_int4", party=party1))
+    e = SecretInteger(Input(name="my_int5", party=party1))
+    f = SecretInteger(Input(name="my_int6", party=party1))
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
+  # Getting the X value 
+    result_x = e*d
+    result_y = result_x - b
+    result_z = result_y * f
+
+  #Getting the Y value
+    result_p = a * f
+    result_q = result_p - e
+    result_r = result_q * c
+
+  # Adding the X value and Y value of Linear Equation
+    result = result_z + result_r
     # make sure you change the output below to be your new output
 
-    return [Output(my_int1, "my_output", party1)]
+    return [Output(result, "my_output", party1)
+    ]
